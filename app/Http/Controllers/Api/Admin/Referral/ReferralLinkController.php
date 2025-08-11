5cbf7bc886bd148befd96e21d96c984457473d24
ibrahimrizq87 <ibrahimrizqabd@gmail.com>
2025-08-11 17:39:57 +0300

    update

--- app/Http/Controllers/Api/Admin/Referral/ReferralLinkController.php
+++ app/Http/Controllers/Api/Admin/Referral/ReferralLinkController.php
@@ -239,6 +239,52 @@ class ReferralLinkController extends BaseController
 
 
 
+    public function getNotReserved(Request $request)
+    {
+
+        $searchTerm = trim($request->input('searchTerm', ''));
+
+        $query = ReferralLink::whereDoesntHave('referralEarning');
+        if ($searchTerm) {
+                $query->where(function ($q) use ($searchTerm) {
+                    $q->where('id', 'LIKE', "%$searchTerm%")
+                        ->orWhere('link', 'LIKE', "%$searchTerm%")
+                        ->orWhere('link_code', 'LIKE', "%$searchTerm%");
+
+                });
+        }
+
+        $page = $request->input('page', 1);
+        $perPage = $request->input('per_page', 10);
+        $data = $query->paginate($perPage, ['*'], 'page', $page);
+
+        $codes = $data->map(function ($item) {
+            return [
+                'id' => $item->id,
+                'link' => $item->link,
+                'link_code' => $item->link_code,
+
+            ];
+        });
+
+        $pagination = [
+            'total' => $data->total(),
+            'current_page' => $data->currentPage(),
+            'per_page' => $data->perPage(),
+            'last_page' => $data->lastPage(),
+        ];
+
+        return jsonResponse(
+            true,
+            200,
+            __('messages.success'),
+            $codes,
+            $pagination
+        );
+     
+    }
+
+
 
 
 }
