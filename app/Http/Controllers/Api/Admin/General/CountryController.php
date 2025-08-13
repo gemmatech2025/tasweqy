bd9832e81a8ec7600aba1ca67d6f58cebde118c7
ibrahimrizq87 <ibrahimrizqabd@gmail.com>
2025-08-13 14:25:17 +0300

    updates

--- app/Http/Controllers/Api/Admin/General/CountryController.php
+++ app/Http/Controllers/Api/Admin/General/CountryController.php
@@ -66,6 +66,83 @@ class CountryController extends BaseController
 
 
 
+    public function index(Request $request)
+{
+
+    $searchTerm = trim($request->input('searchTerm', ''));
+    $filters = $request->input('filter', []);
+    $sortBy = $request->input('sort_by', 'id');
+    $sortOrder = $request->input('sort_order', 'asc');
+    $query = $this->getModel()->with($this->getRelations());
+    $columns = \Schema::getColumnListing($this->getModel()->getTable());
     
+
+    $filters = array_map(function ($value) {
+        if (is_string($value)) {
+            $lower = strtolower($value);
+            return match ($lower) {
+                'true' => 1,
+                'false' => 0,
+                default => is_numeric($value) ? $value + 0 : $value,
+            };
+        }
+        return $value;
+    }, $filters);
+
+    $filters = array_filter($filters, fn($value) => $value !== null && $value !== '');
+
+    foreach ($filters as $key => $value) {
+        if (in_array($key, $columns)) {
+            $query->where($key, $value);
+        }
+    }
+
+
+
+    if (!empty($searchTerm)) {
+        $searchableFields = $this->getSearchableFields();
+        $query->where(function ($q) use ($searchableFields, $searchTerm) {
+            foreach ($searchableFields as $field) {
+                $q->orWhere($field, 'LIKE', "%{$searchTerm}%");
+            }
+        });
+    }
+
+    if ($sortBy && $sortOrder) {
+        $query->orderBy($sortBy, $sortOrder);
+    } else {
+        foreach ($this->getSort() as $sort) {
+            $query->orderBy($sort['sort'], $sort['order']);
+        }
+    }
+
+    if ($this->indexPaginat()) {
+        $page = $request->input('page', 1);
+        $perPage = $request->input('per_page', 10);
+        $data = $query->paginate($perPage, ['*'], 'page', $page);
+
+        $pagination = [
+            'total' => $data->total(),
+            'current_page' => $data->currentPage(),
+            'per_page' => $data->perPage(),
+            'last_page' => $data->lastPage(),
+        ];
+
+        return jsonResponse(
+            true,
+            200,
+            __('messages.success'),
+            (static::RESOURCE)::collection($data),
+            $pagination
+        );
+    }
+
+    return jsonResponse(
+        true,
+        200,
+        __('messages.success'),
+        (static::RESOURCE)::collection($query->get())
+    );
+}
 
 }
